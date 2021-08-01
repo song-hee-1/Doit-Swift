0ccbba160f0a200a96b0456b523b7350df654b45
hee <98_han@naver.com>
2021-08-01 21:42:02 +0900

    To make Page Control final

--- PageControl/ViewController.swift
+++ PageControl/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+var images = ["01.png", "02.png", "03.png" , "04.png" , "05.png", "06.png"]
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var pageControl: UIPageControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        pageControl.numberOfPages =  images.count
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.white
+        pageControl.currentPageIndicatorTintColor = UIColor.gray
+        imgView.image = UIImage(named: images[0])
+        
+        
+        
+        
     }
 
-
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        imgView.image = UIImage(named : images[pageControl.currentPage])
+    }
+    
 }
 
