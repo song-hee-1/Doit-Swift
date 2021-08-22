2cd77cecc117056d37082d15baa8ce2c809992ae
hee <98_han@naver.com>
2021-08-22 19:59:22 +0900

    zoom in & out picture to use pinch gesture

--- PinchGesture/ViewController.swift
+++ PinchGesture/ViewController.swift
@@ -8,13 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var txtPinch: UILabel!
+
+    @IBOutlet var imgPinch: UIImageView!
+    
+    
+    var initialFontSize:CGFloat! 
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.doPinch( _:)))
+        self.view.addGestureRecognizer(pinch)
     }
 
+    @objc func doPinch(_ pinch:UIPinchGestureRecognizer) {
+        imgPinch.transform = imgPinch.transform.scaledBy(x: pinch.scale, y: pinch.scale)
+        pinch.scale = 1
+    }
+    
+    
 
 }
 
