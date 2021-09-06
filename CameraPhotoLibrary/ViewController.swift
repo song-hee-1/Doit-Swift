cf237a8840028bcdd3fca44b1181eeddacfed9ee
hee <98_han@naver.com>
2021-09-06 12:27:08 +0900

    final

--- CameraPhotoLibrary/ViewController.swift
+++ CameraPhotoLibrary/ViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 import MobileCoreServices // 다양한 타입들을 정의해 놓은 헤더 파일 추가
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate { // 델리게이터 프로토콜 추가
-    @IBOutlet var imgView: UIImageView!
+    
+    @IBOutlet var imgView1: UIImageView!
+    @IBOutlet var imgView2: UIImageView!
+    @IBOutlet var imgView3: UIImageView!
+    
 
     // UIImagePickerController 인스턴스 변수 생성
     let imagePicker: UIImagePickerController! = UIImagePickerController()
     var captureImage: UIImage! // 사진을 저장할 변수
     var videoURL: URL! // 녹화한 비디오의 URL을 저장할 변수
     var flagImageSave = false // 사진 저장 여부를 나타낼 변수
+    var numImage = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +32,9 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBAction func btnCaptureImageFromCamera(_ sender: UIButton) {
         // 만일 카메라를 사용할 수 있다면
         if(UIImagePickerController.isSourceTypeAvailable(.camera)) {
+            numImage = numImage + 1
+            if numImage>3 { numImage = 1 }
+            
             flagImageSave = true // 사진 저장 플래그를 true로 설정
             
             imagePicker.delegate = self // 이미지 피커의 델리게이트를 self로 설정
@@ -43,6 +51,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     // 사진 불러오기
     @IBAction func btnLoadImageFromLibrary(_ sender: UIButton) {
         if (UIImagePickerController.isSourceTypeAvailable(.photoLibrary)) {
+            numImage = numImage + 1
+            if numImage>3 { numImage = 1 }
             flagImageSave = false
             
             imagePicker.delegate = self
@@ -57,44 +67,20 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         }
     }
     
-    // 비디오 촬영하기
-    @IBAction func btnRecordVideoFromCamera(_ sender: UIButton) {
-        if (UIImagePickerController.isSourceTypeAvailable(.camera)) {
-            flagImageSave = true
-            
-            imagePicker.delegate = self
-            imagePicker.sourceType = .camera
-            imagePicker.mediaTypes = [kUTTypeMovie as String] // 미디어 타입을 KUTTypeMovie로 설정
-            imagePicker.allowsEditing = false
-            
-            present(imagePicker, animated: true, completion: nil)
-        } else {
-            myAlert("Camera inaccessable", message: "Application cannot access the camera.")
-        }
-    }
-   
-    // 비디오 불러오기
-    @IBAction func btnLoadVideoFromLibrary(_ sender: UIButton) {
-        if (UIImagePickerController.isSourceTypeAvailable(.photoLibrary)) {
-            flagImageSave = false
-            
-            imagePicker.delegate = self
-            imagePicker.sourceType = .photoLibrary
-            imagePicker.mediaTypes = [kUTTypeMovie as String]
-            imagePicker.allowsEditing = false
-            
-            present(imagePicker, animated: true, completion: nil)
-        } else {
-            myAlert("Photo Album inaccessable", message: "Application cannot access the Photo Album.")
-        }
+  
+    @IBAction func btnReset(_ sender: UIButton) {
+            numImage = 0
+            imgView1 = nil
+            imgView2 = nil
+            imgView3 = nil
     }
     
+    
     // 사진,비디오 촬영이나 선택이 끝났을 때 호출되는 델리게이트 메서드
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         // 미디어 종류 확인
-        let mediaType = info[UIImagePickerController.InfoKey.mediaType]
-        as! NSString
-        // 미디어가 사진이면
+        let mediaType = info[UIImagePickerController.InfoKey.mediaType] as! NSString
+   
         if mediaType.isEqual(to: kUTTypeImage as NSString as String) {
             // 사진을 가져옴
             captureImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
@@ -104,7 +90,10 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
                 UIImageWriteToSavedPhotosAlbum(captureImage, self, nil, nil)
             }
             
-            imgView.image = captureImage // 가져온 사진을 이미지뷰에 출력
+            if numImage==1 { imgView1.image = captureImage }
+            else if numImage==2 { imgView2.image = captureImage }
+            else if numImage==3 { imgView3.image = captureImage }
+                // 가져온 사진을 이미지뷰에 출력
         }
         else if mediaType.isEqual(to: kUTTypeMovie as NSString as String) { //미디어가 비디오일 때
                 if flagImageSave { // flagImageSave가 true일 떄
@@ -121,6 +110,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     // 사진,비디오 촬영이나 선택을 취소했을 때 호출되는 델리게이트 메서드
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         //현재의 뷰(이미지피커)제거
+          numImage = numImage - 1
+          if numImage<0 { numImage = 0 }
         self.dismiss(animated: true, completion:nil)
     }
         
